0024e62d6df9bc939595c563a2b777ddfd43ee7f
Guilherme Fantuci <gqueiroz@softgran.com.br>
2022-05-03 11:28:23 -0400

    Aula 3.12

--- escola/settings.py
+++ escola/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
+    'rest_framework',
     'cursos',
 ]
 
@@ -126,3 +128,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# DRF
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',  # Autenticação de sessão
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        # Somente leitura para usuários não autenticados
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    )
+}
--- escola/urls.py
+++ escola/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # URL para autenticação do Django Rest Framework
+    path('auth/', include('rest_framework.urls')),
 ]

26014ef5c4ab9b889e5d2a368c3e878efbad890a
Guilherme Fantuci <gqueiroz@softgran.com.br>
2022-05-03 23:12:09 -0400

    Aula 4.19 - Sobreescrevendo os metódos generics

--- cursos/views.py
+++ cursos/views.py
@@ -1,5 +1,6 @@
 """Uso da generics views do Django Rest Framework"""
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
@@ -18,7 +19,20 @@ class AvaliacoesAPIView(generics.ListCreateAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
 
+    def get_queryset(self):
+        if self.kwargs.get('curso_pk'):
+            return self.queryset.filter(curso=self.kwargs.get('curso_pk'))
+        return self.queryset.all()
+
 
 class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    # Sobrescrevendo o método get_object para retornar o objeto
+    def get_object(self):
+        if self.kwargs.get('curso_pk'):
+            return get_object_or_404(self.get_queryset(),
+                                     curso=self.kwargs.get('curso_pk'),
+                                     pk=self.kwargs.get('avaliacao_pk'))
+        return get_object_or_404(self.queryset(), pk=self.kwargs.get('avaliacao_pk'))

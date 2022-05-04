5613be34c0e02a545e78515b3d5daa1754ca4c7b
Guilherme Fantuci <gqueiroz@softgran.com.br>
2022-05-03 22:17:38 -0400

    Aula 3.15 - Post

--- cursos/views.py
+++ cursos/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
@@ -12,6 +13,12 @@ class CursoAPIView(APIView):
         serializer = CursoSerializer(cursos, many=True)
         return Response(serializer.data)
 
+    def post(self, request):
+        serializer = CursoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
 class AvaliacaoAPIView(APIView):
     """API de Avaliações da Geek"""
@@ -20,3 +27,9 @@ class AvaliacaoAPIView(APIView):
         avaliacoes = Avaliacao.objects.all()
         serializer = AvaliacaoSerializer(avaliacoes, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AvaliacaoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)

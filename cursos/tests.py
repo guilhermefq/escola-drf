63c571cd955a58609721f21b0a594eb7e33b48a6
Guilherme Fantuci <gqueiroz@softgran.com.br>
2022-05-05 11:53:32 -0400

    Seção 6 - Testando a API

--- cursos/tests.py
+++ cursos/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from faker import Faker
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from .models import Curso
 
 # Create your tests here.
+
+
+class CursosTest(APITestCase):
+    url_base = 'http://localhost:8000/api/v2/'
+    headers = {'Authorization': 'Token 236131ba6d799a790e8dcd88c098437b681217ff'}
+    curso_id = 0
+    faker = Faker()
+
+    def test_get_cursos(self):
+        """
+        Testa se o endpoint de cursos está funcionando corretamente
+        """
+        url = reverse('cursos-list')
+        data = {
+            'titulo': 'Curso de Testes com o PyTest',
+            'url': self.faker.url(),
+        }
+        response = self.client.post(
+            url, data, format='json', headers=self.headers)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Curso.objects.count(), 1)
+        self.assertEqual(Curso.objects.get().titulo, data['titulo'])
---
+++ cursos/tests/test_pytest.py
@@ -0,0 +1,65 @@
+import pytest
+import requests
+from faker import Faker
+
+
+class TestCursos:
+    url_base = 'http://localhost:8000/api/v2/'
+    headers = {'Authorization': 'Token 236131ba6d799a790e8dcd88c098437b681217ff'}
+    curso_id = 0
+    fake = Faker()
+
+    # Deve-se seguir o padrão test_
+    def test_get_cursos(self):
+        res = requests.get(url=self.url_base + 'cursos/', headers=self.headers)
+
+        assert res.status_code == 200
+
+    def test_get_curso(self):
+        res = requests.get(url=self.url_base + 'cursos/1/',
+                           headers=self.headers)
+
+        assert res.status_code == 200
+
+    def test_post_curso(self):
+        novo = {
+            "titulo": "Curso de Testes com o PyTest",
+            "url": self.fake.url(),
+        }
+        res = requests.post(url=self.url_base + 'cursos/',
+                            headers=self.headers, data=novo)
+
+        assert res.status_code == 201
+        assert res.json()['titulo'] == novo['titulo']
+
+        TestCursos.curso_id = res.json()['id']
+
+    def test_put_curso(self):
+        atualizado = {
+            "titulo": "Curso de Flutter Intermediário",
+            "url": self.fake.url(),
+        }
+        res = requests.put(url=self.url_base + f'cursos/{TestCursos.curso_id}/',
+                           headers=self.headers, data=atualizado)
+
+        assert res.status_code == 200
+        assert res.json()['titulo'] == atualizado['titulo']
+
+    def test_put_titulo_curso(self):
+        atualizado = {
+            "titulo": "Curso de Flutter Avançado",
+            "url": self.fake.url(),
+        }
+
+        res = requests.put(
+            url=self.url_base + f'cursos/{TestCursos.curso_id}/', headers=self.headers, data=atualizado)
+
+        assert res.status_code == 200
+        assert res.json()['titulo'] == atualizado['titulo']
+
+    def test_delete_curso(self):
+        res = requests.delete(url=self.url_base + f'cursos/{TestCursos.curso_id}/',
+                              headers=self.headers)
+
+        assert res.status_code == 204
+        assert len(res.content) == 0
